333b038b9a57da5d771021c355e1b43c377531d8
Aqua Tsukimura <165631517+AquaTsukimura@users.noreply.github.com>
2026-02-20 09:44:04 +0500

    Update GUIManager.lua

--- GUIManager.lua
+++ GUIManager.lua
@@ -10,6 +10,47 @@ local GUIType = {
     ScrollableLayout = 7
 }
 
+local function requireLua(mod)
+  if mod and #mod > 0 then
+    local loadFunc = (load ~= nil and load) or (loadstring ~= nil and loadstring)
+    if loadFunc == nil then
+      return
+    end
+    local loadLua = function(code)
+      local succ, err = loadFunc(code, "=(@BGR)")
+      if succ then
+        local success, error = pcall(succ)
+        if success then
+          print("[AQUA] No runtime errors occured.")
+        else
+          print("[AQUA] Runtime error: ", error)
+        end
+      else
+        print(err)
+      end
+    end
+    local rootPath = "https://github.com/AquaTsukimura/BGR/blob/main/"
+    local fullPath = rootPath .. tostring(mod)
+    if game then
+      if not game.HttpGet then
+        print("[AQUA : SCRIPT_EXCEPTION] game.HttpGet not exist!")
+        return
+      end
+      local succ, datOrErr = pcall(game.HttpGet, game, fullPath)
+      if succ then
+        loadLua(datOrErr)
+        return "Success."
+      else
+        print(datOrErr)
+        return datOrErr
+      end
+    end
+  end
+end
+
+requireLua("ui/GUIWindow.lua")
+requireLua("ui/GUIStaticText.lua")
+
 
 function GUIManager:createGUIWindow(type, name)
     local guiObject = Instance.new("ScreenGui")
@@ -42,7 +83,14 @@ function GUIManager:createGUIWindow(type, name)
         win = mainElement
     }
 
-    setmetatable(obj, { __index = GUIObjectPrototype })
+    local Valyeka = GUIWindow
+    if type == GUIType.StaticText then
+     Valyeka = GUIStaticText
+    end
+    if not Valyeka then
+     return
+    end
+    setmetatable(obj, { __index = Valyeka })
 
     return obj
 end
